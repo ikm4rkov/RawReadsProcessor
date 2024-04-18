792ec6644f762e124992a1d53f0a4e672f2dc076
ikm4rkov <130388002+ikm4rkov@users.noreply.github.com>
2024-04-18 17:20:53 +0300

    Update batch_p.sh
    Space splitting, variable output fixed

--- batch_p.sh
+++ batch_p.sh
@@ -3,6 +3,6 @@ cat "$1"
 d1=$(cat "$1"  |  sed 's/[+?\-s][^\)]*\]//g')
 echo ./alpha1 -p -e -t -j $3 -k $2 -d $d1 -l 14 -m 14
 ./alpha1 -p -e -t -j $3 -k $2  -d $d1 -l 14 -m 14
-d2=$(cat "$1"  |  sed 's/[?!<][^\)]*)//g; s/b[^\)]*)/\n/g')
-echo ./alpha2 "$2"_DNA "$2"_RNA $d2
-./alpha2 "$2"_DNA "$2"_RNA $d2
+d2=$(cat "$1"  |  sed 's/[?!<][^\)]*)//g; s/b[^\)]*)/ /g')
+echo ./alpha2 "$2"_DNA "$2"_RNA "$d2"
+./alpha2 "$2"_DNA "$2"_RNA "$d2"
